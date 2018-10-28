566e9721f6bd8928255ffb14b849745537b6cdd7
grantIee <grant.lee.2017@gmail.com>
2018-10-28 09:49:00 -0400

    pa3

--- hw/src/db.py
+++ hw/src/db.py
@@ -1,127 +1,44 @@
-import os
-import json
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
-# From: https://goo.gl/YzypOI
-def singleton(cls):
-    instances = {}
-    def getinstance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
-    return getinstance
+db = SQLAlchemy()
 
-class DB(object):
-    """
-    DB driver for the reddit-like forum app - deals with writing entities
-    to the DB and reading entities from the DB
-    """
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key = True)
+    score = db.Column(db.Integer, default = 0)
+    text = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, nullable= False)
+    comments = db.relationship('Comment', cascade = 'delete')
 
-    def __init__(self):
-        self.conn = sqlite3.connect("./posts.db", check_same_thread=False)
-        self.create_post_table()
-        self.create_comment_table()
+def __init__(self, **kwags):
+    self.text = kwags.get('text', '')
+    self.username = kwags.get('username', '')
 
+def serialize(self):
+    return {
+        'id': self.id,
+        'score': self.score,
+        'text': self.text,
+        'username': self.username
+    }
 
-# ===================================================
-# POST FUNCTIONS
-# ===================================================
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key = True)
+    score = db.Column(db.Integer, default = 0)
+    text = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, nullable= False)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable = False)
 
-    def create_post_table(self):
-        try:
-            self.conn.execute("""
-                CREATE TABLE post (
-                    ID INTEGER PRIMARY KEY,
-                    SCORE INTEGER DEFAULT 0,
-                    TEXT TEXT NOT NULL,
-                    USERNAME TEXT NOT NULL
-                );
-            """)
-        except Exception as e:
-            print(e)
+    def __init__(self, **kwags):
+        self.text = kwags.get('text', '')
+        self.username = kwags.get('username', '')
+        self.task_id = kwags.get('task_id')
 
-    def delete_post_table(self):
-        self.conn.execute('DROP TABLE IF EXISTS post;')
-
-    def get_all_posts(self):
-        cursor = self.conn.execute('SELECT * FROM post;')
-        posts = []
-
-        for row in cursor:
-            posts.append({'id': row[0], 'score': row[1], 'text': row[2], 'username': row[3]})
-
-        return posts
-
-    def insert_post_table(self, text, username):
-        cur = self.conn.cursor()
-        cur.execute('INSERT INTO post (TEXT, USERNAME) VALUES (?, ?);', 
-            (text, username))
-        self.conn.commit()
-        return cur.lastrowid
-
-    def get_post_by_id(self, id):
-        cursor = self.conn.execute('SELECT * FROM post WHERE ID == ?', (id,))
-
-        for row in cursor:
-            return {'id': row[0], 'score': row[1], 'text': row[2], 'username': row[3]}
-
-        return None
-
-    def update_post_by_id(self, id, text):
-        self.conn.execute("""
-            UPDATE post 
-            SET TEXT = ?
-            WHERE ID = ?;
-        """, (text, id))
-        self.conn.commit()
-
-    def delete_post_by_id(self, id):
-        self.conn.execute("""
-            DELETE FROM post
-            WHERE id = ?;        
-        """, (id,))
-        self.conn.commit()
-
-
-# ===================================================
-# COMMENT FUNCTIONS
-# ===================================================
-
-    def create_comment_table(self):
-        try:
-            self.conn.execute("""
-                CREATE TABLE comment (
-                    ID INTEGER PRIMARY KEY,
-                    SCORE INTEGER DEFAULT 0,
-                    TEXT TEXT NOT NULL,
-                    USERNAME TEXT NOT NULL,
-                    POST_ID INTEGER NOT NULL,
-                    FOREIGN KEY(POST_ID) REFERENCES post(ID)
-                );
-            """)
-        except Exception as e:
-            print(e)
-
-    def delete_comment_table(self):
-        self.conn.execute('DROP TABLE IF EXISTS comment;')
-
-    def get_all_comments(self,id):
-        cursor = self.conn.execute('SELECT comment.ID, comment.SCORE, comment.TEXT, comment.USERNAME FROM comment INNER JOIN post ON comment.POST_ID = post.ID WHERE post.ID = ?;', (id,))
-        comments = []
-
-        for row in cursor:
-            comments.append({'id': row[0], 'score': row[1], 'text': row[2], 'username': row[3]})
-
-        return comments
-    
-    def insert_post_comment(self, text, username, id):
-        cur = self.conn.cursor()
-        cur.execute('INSERT INTO comment (TEXT, USERNAME, POST_ID) VALUES (?, ?, ?);', 
-            (text, username, id))
-        self.conn.commit()
-        return cur.lastrowid
-        
-
-# Only <=1 instance of the DB driver
-# exists within the app at all times
-DB = singleton(DB)
+    def serialize(self):
+        return {
+            'id': self.id,
+            'score': self.score,
+            'text': self.text,
+            'username': self.username
+        }
--- hw/src/routes.py
+++ hw/src/routes.py
@@ -1,32 +1,45 @@
 from flask import Flask, request
 from flasgger import Swagger
 import json
-import db
+from db import db, Comment, Post
 
+db_filename = 'posts.db'
 app = Flask(__name__)
 app.config['SWAGGER'] = {
-    "title": "CUAppDev - CS1998: Reddit-like Website API",
+    "title": "Cornell AppDev - CS1998: Reddit-like Website API",
     "version": "0.0.1",
-    "description": "This is the API for CUAppDev Fall 2018 Course CS1998: Intro to Backend Development. This API is for a reddit-like forum website. Some links for this API: [Course Github](https://github.com/appdev-courses) | [Course Website](https://github.com/appdev-courses) | [Course Piazza](https://piazza.com/class/jmb59vq5rqv2c5)",
+    "description": "This is the API for Cornell AppDev Fall" + 
+    "2018 Course CS1998: Intro to Backend Development. This API" + 
+    "is for a reddit-like forum website." + 
+    "Some links for this API: [Course Github](https://github.com/appdev-courses)" +
+    "| [Course Website](https://github.com/appdev-courses)" + 
+    "| [Course Piazza](https://piazza.com/class/jmb59vq5rqv2c5)"
 }
 
 swagger = Swagger(app)
 
-Db = db.DB()
+# Configuration of SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
-####Code Begins Here#### 
+# Initiation of db
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+#### Code Begins Here #### 
 
 @app.route('/api/posts/')
 def get_posts():
     '''
     file: ./documentation/get_posts.yml
     '''
-
-    res = {'success': True, 'data': Db.get_all_posts()}
+    posts = Post.query.all()
+    res = {'success': True, 'data': [posts.serialize() for post in posts]}
     return json.dumps(res), 200
 
-#DONE
-@app.route('/api/posts/', methods=['POST'])
+@app.route('/api/posts/', methods = ['POST'])
 def create_post():
     '''
     file: ./documentation/create_post.yml
@@ -35,57 +48,54 @@ def create_post():
     request_body = json.loads(request.data)
     #Code here checks for blank body requests / @beforerequests checks for None body requests
     if not request_body['text'] == '' and not request_body['username'] == '':
-        text = request_body['text']
-        username = request_body['username']
-        post = {
-            'id': Db.insert_post_table(text, username),
-            'score': 0, 
-            'text': text,
-            'username': username
-        }
-        #INSERT CODE FOR CREATING TABLE WITH COMMENTS
+        post = Post(
+            text = request_body.get('text'),
+            username = request_body.get('username')
+        )
+        # Keep the two acts separate to reduce automatic commits to the server
+        db.session.add(post)
+        db.session.commit()
         return json.dumps({'success': True, 'data': post}), 201
     return json.dumps({'success': False, 'error': 'invalid body format'}), 412
 
-#DONE
 @app.route('/api/post/<int:post_id>/')
 def get_post(post_id):
     '''
     file: ./documentation/get_post.yml
     '''
 
-    post = Db.get_post_by_id(post_id)
+    post = Post.query.filter_by(id = post_id).first()
     if post is not None:
-        return json.dumps({'success': True, 'data': post}), 200
+        return json.dumps({'success': True, 'data': post.serialize()}), 200
     return json.dumps({'success': False, 'error': 'Post not found!'}), 404
 
-#DONE
-@app.route('/api/post/<int:post_id>/', methods=['POST'])
+@app.route('/api/post/<int:post_id>/', methods = ['POST'])
 def update_post(post_id):
     '''
     file: ./documentation/edit_post.yml
     '''
 
-    request_body = json.loads(request.data)
-    text = request_body['text']
-    Db.update_post_by_id(post_id, text)
-
-    post = Db.get_post_by_id(post_id)
+    post = Post.query.filter_by(id = post_id).first()
     if post is not None:
-        return json.dumps({'success': True, 'data': post}), 200
+        request_body = json.loads(request.data)
+        if not request_body['text'] == '' and not request_body['username'] == '':
+            post.text = request_body.get('text', post.text)
+            post.username = request_body.get('username', post.username)
+            db.session.commit()
+        return json.dumps({'success': True, 'data': post.serialize()}), 200
     return json.dumps({'success': False, 'error': 'Post not found!'}), 404
 
-#DONE
-@app.route('/api/post/<int:post_id>/', methods=['DELETE'])
+@app.route('/api/post/<int:post_id>/', methods = ['DELETE'])
 def delete_post(post_id):
     '''
     file: ./documentation/delete_post.yml
     '''
 
-    post = Db.get_post_by_id(post_id)
+    post = Post.query.filter_by(id = post_id).first()
     if post is not None:
-        Db.delete_post_by_id(post_id)
-        return json.dumps({'success': True, 'data': post}), 200
+        db.session.delete(post)
+        db.session.commit()
+        return json.dumps({'success': True, 'data': post.serialize()}), 200
     return json.dumps({'success': False, 'error': 'Post not found!'}), 404
 
 @app.route('/api/post/<int:post_id>/comments/')
@@ -94,36 +104,38 @@ def get_comments(post_id):
     file: ./documentation/get_comments.yml
     '''
 
-    if Db.get_post_by_id(post_id) is not None:
-        comments = Db.get_all_comments(post_id)
+    post = Post.query.filter_by(id = post_id).first()
+    if post is not None:
+        comments = [comment.serialize() for comment in post.comments]
         if len(comments) != 0:
             return json.dumps({'success': True, 'data': comments}), 200
         return json.dumps({'success': True, 'data': 'There are no comments for this post.'}), 200
     return json.dumps({'success': False, 'error': 'Post not found!'}), 404
 
-@app.route('/api/post/<int:post_id>/comment/', methods=['POST'])
+@app.route('/api/post/<int:post_id>/comment/', methods = ['POST'])
 def create_comment(post_id):
     '''
     file: ./documentation/create_comment.yml
     '''
 
-    request_body = json.loads(request.data)
-    #Code here checks for blank body requests / @beforerequests checks for None body requests
-    if not request_body['text'] == '' and not request_body['username'] == '':
-        text = request_body['text']
-        username = request_body['username']
-        if Db.get_post_by_id(post_id) is not None:
-            comment = {
-                'id': Db.insert_post_comment(text, username, post_id),
-                'score': 0, 
-                'text': text,
-                'username': username
-            }
-            return json.dumps({'success': True, 'data': comment}), 201
+    post = Post.query.filter_by(id = post_id).first()
+    if Db.get_post_by_id(post_id) is not None:
+        request_body = json.loads(request.data)
+        # Code here checks for blank body requests / @beforerequests checks for None body requests
+        if not request_body['text'] == '' and not request_body['username'] == '':
+            comment = Comment(
+                text = request_body.get('text'),
+                username = request_body.get('username'),
+                post_id = post_id
+            )
+            post.comments.append(comment)
+            db.session.add(comment)
+            db.session.commit()
+            return json.dumps({'success': True, 'data': comment.serialize()}), 201
         return json.dumps({'success': False, 'error': 'Post not found!'}), 404
     return json.dumps({'success': False, 'error': 'invalid body format'}), 412
 
-#Code that checks for post requests that submit a None body
+# Code that checks for post requests that submit a None body
 @app.before_request
 def before_request():
     if request.method == 'POST':
